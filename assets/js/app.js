ee00cac974e77a1bdb01dbd59da8d8b9342dd517
Nawal Nasarullah <nawalisbd@gmail.com>
2023-10-11 01:14:52 +0500

    -------------------------

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,3 +41,50 @@ $('[data-carousel-id="carousel2"]').owlCarousel({
         }
     }
 })
+
+
+$('[data-carousel-id="carousel3"]').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+const targetDate = new Date(2023, 10, 31, 23, 59, 59); 
+
+function updateCountdown() {
+    const countdownElement = document.getElementById("countdown");
+    const currentDate = new Date();
+    const timeRemaining = targetDate - currentDate;
+
+    if (timeRemaining <= 0) {
+        countdownElement.textContent = "Sale has ended!";
+    } else {
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        countdownElement.innerHTML = `
+        <i class="material-symbols-outlined icon">schedule</i>  <span class="expire" >Expires in</span>: ${days} : ${hours} : ${minutes} : ${seconds}  `;
+    }
+}
+
+
+updateCountdown();
+
+const timerInterval = setInterval(updateCountdown, 1000);
