77725364bd89764e691898f0fdfd19b6d83d6faf
Nawal Nasarullah <nawalisbd@gmail.com>
2023-10-12 19:09:01 +0500

    ------------------

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,21 +89,24 @@ updateCountdown();
 
 const timerInterval = setInterval(updateCountdown, 1000);
 
-const decrementButton = document.getElementById("decrement");
-        const incrementButton = document.getElementById("increment");
-        const itemCount = document.getElementById("item-count");
+const decrementButtons = document.querySelectorAll(".decrement");
+const incrementButtons = document.querySelectorAll(".increment");
+const itemCounts = document.querySelectorAll(".item-count");
 
-        let count = 0;
+decrementButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        let count = parseInt(itemCounts[index].innerText, 10);
+        if (count > 0) {
+            count--;
+            itemCounts[index].innerText = count;
+        }
+    });
+});
 
-        // Decrement button click handler
-        decrementButton.addEventListener("click", () => {
-            if (count > 0) {
-                count--;
-                itemCount.innerText = count;
-            }
-        });
-
-        incrementButton.addEventListener("click", () => {
-            count++;
-            itemCount.innerText = count;
-        });
+incrementButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        let count = parseInt(itemCounts[index].innerText, 10);
+        count++;
+        itemCounts[index].innerText = count;
+    });
+});
